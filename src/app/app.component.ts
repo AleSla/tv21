fc283b8000a891e9cf4ab26da053ecfcb7b8017e
alex49136 <alex49136@gmail.com>
2024-06-19 08:48:36 +0000

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,12 @@ import { MatToolbar } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbar, MatIconModule, FlexLayoutModule, 
+  imports: [RouterOutlet, MatToolbar, MatIconModule, 
     MatButtonModule, RouterLink, RouterLinkActive, MatSidenavModule, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
